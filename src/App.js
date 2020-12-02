d46531316494fc064b5ea227ecaf844e16d93cdd
Jonathan Warner <jewarner57@gmail.com>
2020-12-02 18:04:12 -0500

    finished required challenges

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import React, { useState } from 'react'
 function App() {
     const [total, setTotal] = useState(0);
     const [percent, setPercent] = useState(0);
+    const [split, setSplit] = useState(1);
 
     return (
         <div className="App">
             <h1>Tip Calculator</h1>
-            <TextInput name="Total" onChange={(value) => setTotal(value)} />
-            <TextInput name="Percent" onChange={(value) => setPercent(value)} />
-            <h1>{total} * {percent * 0.01}</h1>
-            <h1>Tip Total: ${Math.round(100 * total * (percent * 0.01)) / 100}</h1>
+            <TextInput name="Bill Total" onChange={(value) => setTotal(value)} />
+            <TextInput name="Tip Percent" onChange={(value) => setPercent(value)} />
+            <TextInput name="How many was to split?" onChange={(value) => setSplit(value)} />
+            <h1>Per Person Amount: ${(100 * total * (percent * 0.01) / 100 + parseInt(total) / split)}</h1>
+            <h1>Check Total: ${Math.round(100 * total * (percent * 0.01)) / 100 + parseInt(total)}</h1>
+
         </div>
     );
 }
